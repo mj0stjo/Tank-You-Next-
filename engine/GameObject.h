a0857ab25a249fce5663a81a4eaf23c4583d483d
Daniel Stempfle <stempfledaniel@gmx.de>
2023-04-20 17:52:13 +0200

    Add GameObject and Tank

---
+++ engine/GameObject.h
@@ -0,0 +1,30 @@
+#ifndef GAMEOBJECT_H
+#define GAMEOBJECT_H
+
+// Include OpenGL Stuff
+#include <glm/glm.hpp>
+#include <GL/glew.h>
+
+// std includes
+#include <string>
+
+class GameObject {
+	public:
+		GameObject();
+		virtual ~GameObject();
+		virtual void update() = 0;
+		virtual void render() = 0;
+		virtual bool onCollisionEnter() = 0;
+	protected:
+		glm::vec3 position;
+		glm::vec3 rotation;
+		glm::vec3 scale;
+		GLuint programID;
+		void parseStl(std::string path);
+		virtual void inializeBuffers() = 0;
+		virtual void cleanupBuffers() = 0;
+		
+		
+};
+
+#endif // !GAMEOBJECT_H
